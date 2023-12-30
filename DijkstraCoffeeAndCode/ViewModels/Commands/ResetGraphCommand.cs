6546601bb1d85e04e16ad0ae91122c33f003a1f0
Corey <Corey255A1@hotmail.com>
2023-12-30 08:36:14 -0500

    Beginnings of X/Y Edit fields

---
+++ DijkstraCoffeeAndCode/ViewModels/Commands/ResetGraphCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DijkstraCoffeeAndCode.ViewModels.Commands
+{
+    public class ResetGraphCommand : ICommand
+    {
+        private GraphViewModel _viewModel;
+        public event EventHandler? CanExecuteChanged;
+
+        public ResetGraphCommand(GraphViewModel viewModel)
+        {
+            _viewModel = viewModel;
+            _viewModel.PropertyChanged += ViewModelPropertyChanged;
+        }
+
+        private void ViewModelPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_viewModel.SelectedExecutionMode))
+            {
+                CanExecuteChanged?.Invoke(this, new EventArgs());
+            }
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return _viewModel.SelectedExecutionMode == AlgorithmExecutionModeEnum.Manual;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _viewModel.ResetAllDijkstraViewObjects();
+        }
+    }
+}
---
+++ DijkstraCoffeeAndCode/Views/NodeProperties.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DijkstraCoffeeAndCode.Views
+{
+    /// <summary>
+    /// Interaction logic for NodeProperties.xaml
+    /// </summary>
+    public partial class NodeProperties : UserControl
+    {
+        public NodeProperties()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControlKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                DependencyObject ancestor = Parent;
+                while (ancestor != null)
+                {
+                    var element = ancestor as UIElement;
+                    if (element != null && element.Focusable)
+                    {
+                        element.Focus();
+                        break;
+                    }
+
+                    ancestor = VisualTreeHelper.GetParent(ancestor);
+                }
+            }
+        }
+    }
+}
