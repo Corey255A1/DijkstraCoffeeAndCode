b88ceb29bf29ae50c4d2feb57bb1aee137fcfade
Corey <Corey255A1@hotmail.com>
2024-01-07 15:15:05 -0500

    Cleaned up theme handling a bit more

--- DijkstraCoffeeAndCode/MainWindow.xaml.cs
+++ DijkstraCoffeeAndCode/MainWindow.xaml.cs
@@ -11,146 +11,43 @@ using System.Windows.Input;
 namespace DijkstraCoffeeAndCode
 {
     public partial class MainWindow : Window
-    {
-
-        public DijkstraGraphViewModel Graph { get; private set; }
-
-        public GraphFileManager FileManager { get; private set; }
-
-        public double ViewWidth
-        {
-            get { return (double)GetValue(ViewWidthProperty); }
-            set { SetValue(ViewWidthProperty, value); }
-        }
-
-        public static readonly DependencyProperty ViewWidthProperty =
-            DependencyProperty.Register("ViewWidth", typeof(double), typeof(MainWindow), new PropertyMetadata(null));
-
-
-        public double ViewHeight
-        {
-            get { return (double)GetValue(ViewHeightProperty); }
-            set { SetValue(ViewHeightProperty, value); }
-        }
-
-        public static readonly DependencyProperty ViewHeightProperty =
-            DependencyProperty.Register("ViewHeight", typeof(double), typeof(MainWindow), new PropertyMetadata(null));
-
-        private const int VIEW_SIZE = 2048;
-        private double _zoomLevel = 0.9;
-        public double ZoomLevel
-        {
-            get => _zoomLevel;
-            set
-            {
-                _zoomLevel = value;
-                if (_zoomLevel > 1.5) { _zoomLevel = 1.5; }
-                else if (_zoomLevel < 0.5) { _zoomLevel = 0.5; }
-
-                ViewHeight = VIEW_SIZE * _zoomLevel;
-                ViewWidth = VIEW_SIZE * _zoomLevel;
-            }
-        }
-
-        public string NextStyleName
-        {
-            get { return (string)GetValue(NextStyleNameProperty); }
-            set { SetValue(NextStyleNameProperty, value); }
-        }
-
-        public static readonly DependencyProperty NextStyleNameProperty =
-            DependencyProperty.Register("NextStyleName", typeof(string), typeof(MainWindow), new PropertyMetadata("Night Mode"));
-
+    {        
+        public MainViewModel _viewModel { get; set; }
 
         public MainWindow()
         {
-            ViewWidth = VIEW_SIZE;
-            ViewHeight = VIEW_SIZE;
-            ZoomLevel = 0.8;
-            Graph = new DijkstraGraphViewModel();
-            FileManager = new GraphFileManager(Graph);
-            FileManager.GetFilePath = GetFilePath;
-            Graph.MessageEvent += GraphMessageEvent;
-            FileManager.MessageEvent += GraphMessageEvent;
-            DataContext = Graph;
+            _viewModel = new MainViewModel();
+            DataContext = _viewModel;
             InitializeComponent();
         }
-
-        private void GraphMessageEvent(string message)
+        private bool AddNodeKeyDown()
         {
-            MessageBox.Show(message);
+            return Keyboard.GetKeyStates(Key.LeftCtrl).HasFlag(KeyStates.Down) ||
+                Keyboard.GetKeyStates(Key.RightCtrl).HasFlag(KeyStates.Down);
         }
-
-        private string GetFilePath(bool isOpen, string extensions)
-        {
-            if (isOpen)
-            {
-                OpenFileDialog openFileDialog = new OpenFileDialog();
-                openFileDialog.Filter = extensions;
-                if (openFileDialog.ShowDialog() == true)
-                {
-                    return openFileDialog.FileName;
-                }
-            }
-            else
-            {
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = extensions;
-                if (saveFileDialog.ShowDialog() == true)
-                {
-                    return saveFileDialog.FileName;
-                }
-            }
-            return "";
-        }
-
-        private void CanvasMouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        {
-        }
-
         private void CanvasMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if(!AddNodeKeyDown()) { return; }
             Point clickPoint = e.GetPosition(sender as FrameworkElement);
-            Graph.MakeNewNodeCommand.Execute(new Vector2D(clickPoint.X, clickPoint.Y));
-        }
-
-        private void ScaleZoom(double scale)
-        {
-            ZoomLevel *= scale;
-        }
+            if (_viewModel.Graph.MakeNewNodeCommand.CanExecute(null))
+            {
+                _viewModel.Graph.MakeNewNodeCommand.Execute(new Vector2D(clickPoint.X, clickPoint.Y));
+            }
+        }        
 
         private void ZoomOutClick(object sender, RoutedEventArgs e)
         {
-            ScaleZoom(0.8);
+            _viewModel.ScaleZoom(0.8);
         }
 
         private void ZoomInClick(object sender, RoutedEventArgs e)
         {
-            ScaleZoom(1.2);
+            _viewModel.ScaleZoom(1.2);
         }
-        private void SetNewColorScheme(string colorFile, string previousColorFile)
+
+        private void DayNightModeClick(object sender, RoutedEventArgs e)
         {
-            var dictionary = Application.Current.Resources.MergedDictionaries.FirstOrDefault(resource => resource.Source.OriginalString.Contains(previousColorFile));
-            if (dictionary != null)
-            {
-                Application.Current.Resources.MergedDictionaries.Remove(dictionary);
-            }
-            ResourceDictionary newColorScheme = new();
-            newColorScheme.Source = new Uri(colorFile, UriKind.Relative);
-            Application.Current.Resources.MergedDictionaries.Add(newColorScheme);
-        }
-        private void ColorButtonClick(object sender, RoutedEventArgs e)
-        {
-            if (NextStyleName == "Night Mode")
-            {
-                SetNewColorScheme("DarkColors.xaml", "DefaultColors.xaml");
-                NextStyleName = "Day Mode";
-            }
-            else
-            {
-                SetNewColorScheme("DefaultColors.xaml", "DarkColors.xaml");
-                NextStyleName = "Night Mode";
-            }
+            _viewModel.MoveToNextColorScheme();
         }
     }
 }
---
+++ DijkstraCoffeeAndCode/Resources/Themes/Themes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DijkstraCoffeeAndCode.Resources.Themes
+{
+    public static class Themes
+    {
+        public static readonly Dictionary<string, string> ColorSchemeMap = new() {
+            { "Day Mode", "Resources/Themes/DefaultColors.xaml" },
+            { "Night Mode", "Resources/Themes/DarkColors.xaml" }
+        };
+
+        public static readonly List<KeyValuePair<string, string>> ColorSchemeList = ColorSchemeMap.ToList();
+
+        public static int GetCurrentThemeIndex(Collection<ResourceDictionary> resources)
+        {
+            ResourceDictionary? resource = resources.FirstOrDefault((dictionary) => { return ColorSchemeMap.Values.Contains(dictionary.Source.OriginalString); });
+            if (resource == null) { throw new Exception("Theme not found in ColorSchemeMap"); }
+
+            return ColorSchemeList.FindIndex(keyValue => keyValue.Value == resource.Source.OriginalString);
+        }
+
+        public static int GetNext(int index, out KeyValuePair<string, string> colorScheme)
+        {
+            index += 1;
+            if (index < 0 || index >= ColorSchemeList.Count)
+            {
+                colorScheme = ColorSchemeList[0];
+                return 0;
+            }
+
+            colorScheme = ColorSchemeList[index];
+            return index;
+        }
+    }
+}
